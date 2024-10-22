4b0b53cef4f3c70104d08c8dc6c7ac67449a11f4
Valentina Garcia <valentinagarciaalfonso@gmail.com>
2024-10-21 23:10:24 -0500

    Carpetas

--- src/main/java/com/proyecto3ld/Main.java
+++ src/main/java/com/proyecto3ld/Main.java
@@ -12,11 +12,11 @@ import src.main.java.com.proyecto3ld.util.FuzzyLoad;
 public class Main {
     public static void main(String[] args) {
         // Cargar variables desde archivo
-        List<LingVariable> variables = FuzzyLoad.loadVariablesFromFile("variables.txt");
+        List<LingVariable> variables = FuzzyLoad.loadVariablesFromFile("src/main/resources/variables.txt");
 
         // Crear base de conocimiento
         KnowledgeBase knowledgeBase = new KnowledgeBase();
-        knowledgeBase.loadRulesFromFile("rules.txt");
+        knowledgeBase.loadRulesFromFile("src/main/resources/rules.txt");
         
         // Crear motor de inferencia
         FuzzyInference inference = new FuzzyInference(knowledgeBase);
